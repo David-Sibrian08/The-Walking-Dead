e899ac5f95b36ebee2ed47bbbc0dd98cac29adb5
David Sibrian <davidsibrian08@gmail.com>
2016-09-04 16:38:51 -0400

    Finished the scrolling of the dolls on the screen

--- TheWalkingDead/ViewController.swift
+++ TheWalkingDead/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    //the height and width of the image view where the characters will be placed
+    let WIDTH: CGFloat = UIScreen.mainScreen().fixedCoordinateSpace.bounds.width
+    let HEIGHT: CGFloat = 397
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 1...5 {
+            let img = UIImage(named: "\(i)")
+            let imgView = UIImageView(image: img)
+            
+            //picture has been added to the view. 
+            //Positioning and size need fixing
+            scrollView.addSubview(imgView)
+            imgView.contentMode = UIViewContentMode.ScaleAspectFit
+            
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(i)), 185, WIDTH, HEIGHT)
+        }
+        
+        //5 characters so the scroll view size is WIDTH * 5, and keep the same height
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height - 35)
+        
     }
 
     override func didReceiveMemoryWarning() {
